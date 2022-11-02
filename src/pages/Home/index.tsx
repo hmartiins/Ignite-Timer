1864360c4575c292cb91a5269751b40b489076e2
Henrique Martins <hmartins224@gmail.com>
2022-11-02 20:36:45 -0300

    solving countdown bug and changing page title to countdown time

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -49,12 +49,17 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   useEffect(() => {
+    let interval: number
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -66,6 +71,7 @@ export function Home() {
       startDate: new Date(),
     }
 
+    setAmountSecondsPassed(0)
     setActiveCycleId(newCycle.id)
     setCycles((state) => [...state, newCycle])
 
@@ -80,6 +86,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [activeCycle, minutes, seconds])
 
   const task = watch('task')
   const isSubmitDisabled = !task
